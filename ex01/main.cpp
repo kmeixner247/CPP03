e37de16afec4580cd1259319d2059ba21720959b
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-06-26 20:43:09 +0200

    changed Liza to liza because.. I don't even know

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,20 +6,20 @@
 /*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:54:40 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/06/26 20:33:29 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/06/26 20:42:47 by kmeixner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
 int main() {
-	ScavTrap Liza("Liza");
-	Liza.attack("nothing in particular");
-	Liza.takeDamage(58);
-	Liza.attack("nothing in particular");
-	Liza.beRepaired(23);
-	Liza.takeDamage(100);
-	Liza.attack("nothing in particular");
-	Liza.beRepaired(23);
-	Liza.guardGate();
+	ScavTrap liza("Liza");
+	liza.attack("nothing in particular");
+	liza.takeDamage(58);
+	liza.attack("nothing in particular");
+	liza.beRepaired(23);
+	liza.takeDamage(100);
+	liza.attack("nothing in particular");
+	liza.beRepaired(23);
+	liza.guardGate();
 }
