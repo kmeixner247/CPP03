a26ada66b0beb11b581f6c909b3e6dc48344b0dd
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-06-27 11:37:28 +0200

    added some things to the tests

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 09:11:12 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/06/27 11:09:37 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/06/27 11:36:53 by kmeixner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int main()
 {
 	DiamondTrap billy("Billy");
 	billy.attack("whatever");
+	billy.takeDamage(5);
+	billy.beRepaired(3);
 	billy.guardGate();
 	billy.highFivesGuys();
 	billy.whoAmI();
